ed707760bea9618a20223723476868382c194d58
Honey Kumar <honey06302@gmail.com>
2026-03-19 11:07:40 -0400

    Linting errors 4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -135,6 +135,7 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as e:
             logger.error("Error posting review: %s", e)
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"status": 401,
+                 "message": "Error in posting review"})
 
     return JsonResponse({"status": 403, "message": "Unauthorized"})
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -10,7 +10,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = ("django-insecure-ccow$tz_=9%dxu4(0%"
-                "^(z%nx32#s@(zt9$ih@)5l54yny)wm-0")
+            "^(z%nx32#s@(zt9$ih@)5l54yny)wm-0")
 
 DEBUG = True
 
@@ -91,13 +91,16 @@ AUTH_PASSWORD_VALIDATORS = [
         ),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation." "MinimumLengthValidator"),
+        "NAME": ("django.contrib.auth.password_validation." 
+        "MinimumLengthValidator"),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation." "CommonPasswordValidator"),
+        "NAME": ("django.contrib.auth.password_validation." 
+        "CommonPasswordValidator"),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation." "NumericPasswordValidator"),
+        "NAME": ("django.contrib.auth.password_validation." 
+        "NumericPasswordValidator"),
     },
 ]
 
